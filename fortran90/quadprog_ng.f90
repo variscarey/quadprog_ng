8883bc8c40f50b51f58959d1cb94a828dffe13f0
anachronicnomad <anachronicnomad@gmail.com>
2019-12-19 15:04:42 -0700

    begin scaffolding fortran90 version

---
+++ fortran90/quadprog_ng.f90
@@ -0,0 +1,13 @@
+module quadprog_ng
+  implicit none
+contains
+  subroutine solve_qp(quadr_coeff_G, linear_coeff_a,
+                      n_ineq, ineq_coef_C, ineq_vec_d,
+                      m_eq, eq_coef_A, eq_vec_b,
+                      sol, ierr)
+    !
+    implicit none
+    !
+
+  end subroutine solve_qp
+end module
