28867045df45c62fd814c1306371ed2a26b3a1b2
anachronicnomad <anachronicnomad@gmail.com>
2019-12-20 05:51:36 -0700

    begin declarations of vars, scaffold

--- fortran90/quadprog_ng.f90
+++ fortran90/quadprog_ng.f90
@@ -8,6 +8,37 @@ contains
     !
     implicit none
     !
+    ! Externals
+    ! 
+    real(8), allocatable, intent(in) :: quadr_coeff_G(:,:)
+    real(8), allocatable, intent(in) :: linear_coeff_a(:)
+    
+    integer, intent(in) :: n_ineq
+    real(8), allocatable, intent(in) :: ineq_coef_C(:,:)
+    real(8), allocatable, intent(in) :: ineq_vec_d(:)
+
+    integer, intent(in) :: m_eq
+    real(8), allocatable, intent(in) :: eq_coef_A
+    real(8), allocatable, intent(in) :: eq_vec_b
+
+    real(8), allocatable, intent(out) :: sol(:)
+    integer, intent(out) :: ierr = 0    
+    ! 
+    ! Internals
+    !
+    logical DONE = .FALSE.
+    logical FULL_STEP = .FALSE. 
+    logical ADDING_EQ_CONSTRAINTS = .TRUE.
+
+    logical first_pass = .TRUE. 
+
+    !!~~~ Begin Processing ~~~!!
+    !! Solution iterate
 
   end subroutine solve_qp
 end module
+
+
+program test
+
+end program test
