5ccf5d19e49ae8fcb0f5433afc1104f779103037
anachronicnomad <anachronicnomad@gmail.com>
2019-12-20 17:26:25 -0700

    begin adding vars

--- fortran90/quadprog_ng.f90
+++ fortran90/quadprog_ng.f90
@@ -4,11 +4,15 @@ contains
   subroutine solve_qp(quadr_coeff_G, linear_coeff_a,
                       n_ineq, ineq_coef_C, ineq_vec_d,
                       m_eq, eq_coef_A, eq_vec_b,
-                      sol, ierr)
+                      nvars, sol, ierr)
     !
     implicit none
-    !
-    ! Externals
+
+    !!
+    !! Externals
+    !! 
+
+    ! quadratic coefficient matrix G in 
     ! 
     real(8), allocatable, intent(in) :: quadr_coeff_G(:,:)
     real(8), allocatable, intent(in) :: linear_coeff_a(:)
@@ -21,19 +25,55 @@ contains
     real(8), allocatable, intent(in) :: eq_coef_A
     real(8), allocatable, intent(in) :: eq_vec_b
 
-    real(8), allocatable, intent(out) :: sol(:)
-    integer, intent(out) :: ierr = 0    
-    ! 
-    ! Internals
-    !
+    integer :: intent(in) :: nvars
+
+    ! the solution iterate
+    real(8), allocatable, intent(inout) :: sol(:)
+
+    ! If ierr is set to anything except for zero, a problem happened
+    integer, intent(inout) :: ierr = 0    
+
+    !!
+    !! Internals
+    !!
     logical DONE = .FALSE.
     logical FULL_STEP = .FALSE. 
     logical ADDING_EQ_CONSTRAINTS = .TRUE.
 
     logical first_pass = .TRUE. 
 
+    !! Cholesky decomp of quadr_coeff_G
+    real(8), allocatable :: chol_L(:,:)
+    real(8), allocatable :: inv_chol_L(:,:)
+
+    !! QR factorization of B = L^{-1} N
+    real(8), allocatable :: Q(:,:)
+    real(8), allocatable :: R(:,:)
+
+    !! J = L^{-T} Q, inverse transpose of L by columns of Q = [Q1 | Q2]
+    !! Q1 has the columns corresponding to active constraints 
+    real(8), allocatable :: J1(:,:)
+    real(8), allocatable :: J2(:,:)
+
+    integer, allocatable :: active_set(:)
+    integer, allocatable :: n_p(:)
+    integer :: p, &
+               q 
+
+    real(8), allocatable :: u(:)
+    real(8), allocatable :: lagr(:)
+
+    integer :: k_dropped, &
+               j_dropped
+
+    real(8), allocatable :: z(:), & 
+                            r(:)
+
+    !!~~~ Allocations ~~~!!
+
     !!~~~ Begin Processing ~~~!!
     !! Solution iterate
+
 
   end subroutine solve_qp
 end module
