00cb7cc30f3f4e66e0547cbfc4a65b0db149a067
anachronicnomad <anachronicnomad@gmail.com>
2019-12-23 14:53:21 -0700

    do cholesky factorization and G inverse

--- fortran90/quadprog_ng.f90
+++ fortran90/quadprog_ng.f90
@@ -25,7 +25,7 @@ contains
     real(8), allocatable, intent(in) :: eq_coef_A
     real(8), allocatable, intent(in) :: eq_vec_b
 
-    integer :: intent(in) :: nvars
+    integer, intent(in) :: nvars
 
     ! the solution iterate
     real(8), allocatable, intent(inout) :: sol(:)
@@ -41,6 +41,12 @@ contains
     logical ADDING_EQ_CONSTRAINTS = .TRUE.
 
     logical first_pass = .TRUE. 
+
+    integer status = 0
+    integer irow, icol = 1
+
+    real(8), allocatable :: G_inv(:,:)
+    real(8), allocatable :: U_work(:,:)
 
     !! Cholesky decomp of quadr_coeff_G
     real(8), allocatable :: chol_L(:,:)
@@ -69,7 +75,66 @@ contains
     real(8), allocatable :: z(:), & 
                             r(:)
 
-    !!~~~ Allocations ~~~!!
+    !!~~~ Allocations & Initializations ~~~!!
+    if (m_eq .eq. 0) then
+      ADDING_EQ_CONSTRAINTS = .true.
+    else
+      ADDING_EQ_CONSTRAINTS = .false. 
+    endif
+
+    if (.not. (allocated(sol))) then
+      allocate(sol(nvars))
+    endif
+
+    allocate(chol_L(nvars, nvars))
+    allocate(inv_chol_L(nvars, nvars))
+
+    !! Begin chol factorization and use the factors to get
+    !! the inverse of the matrix G
+
+    ! Lower triangular cholesky 
+    call dpotrf('L', nvars, L, nvars, status)
+
+    ! Set all non-lower-triangular entries to 0
+    do icol=1,nvars
+        do irow=1,nvars
+            if (irow .lt. icol) then
+                L(irow, icol) = 0
+            endif
+        enddo
+    enddo
+
+    allocate(G_inv(nvars,nvars))
+
+    G_inv = L
+
+    ! calc G^{-1}
+    call dpotri('L', nvars, G_inv, nvars, status)
+
+    allocate(U_work(nvars,nvars))
+
+    U_work = G
+
+    call dpotrf('U', 4, U_work, 4, info)
+    call dpotri('U', 4, U_work, 4, info)
+
+    do icol=1,nvars
+        do irow=1,nvars
+            if (irow .ge. icol) then
+                U_work(irow, icol) = 0
+            endif
+        enddo
+    enddo
+
+    !! Begin adding the lower and upper terms
+
+    do icol=1,nvars
+      do irow=1,nvars
+        G_inv(irow, icol) = G_inv(irow,icol) + U_work(irow,icol)
+      enddo
+    enddo
+    
+
 
     !!~~~ Begin Processing ~~~!!
     !! Solution iterate
